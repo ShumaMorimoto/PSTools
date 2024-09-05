580f1a989955e4af1706618b7f92e3d837f8fd42
ShumaMorimoto <shumamorimoto@gmail.com>
2024-09-06 08:49:31 +0900

    Update OfficeTools.psm1

--- OfficeTools/OfficeTools.psm1
+++ OfficeTools/OfficeTools.psm1
@@ -104,7 +104,6 @@ Class OTDomDAO :System.Xml.XmlDocument {
         return $this.tables
     }
 }
-
 class ExTable :AbstractTable {
     [object] $range
     [hashtable] $eheader = [ordered]@{}
@@ -494,6 +493,9 @@ class ConfluDAO : OTDomDAO {
     static [string] $dtd = @"
 <!DOCTYPE page[
 <!ENTITY nbsp "&#160;">
+<!ENTITY rArr "&#8658;">
+<!ENTITY rarr "&#8594;">
+<!ENTITY times "&#215;">
 <!ATTLIST page xmlns:ci CDATA #FIXED "ci">
 <!ATTLIST page xmlns:li CDATA #FIXED "li">
 <!ATTLIST page xmlns:ac CDATA #FIXED "ac">
@@ -540,6 +542,7 @@ class ConfluDAO : OTDomDAO {
         $this.vernum = $response.version.number
         $this.title = $response.title
                 
+        [ConfluDAO]::toXML($this.page) | Set-Content -Path "h:\tmp\page.xml"
         $this.LoadXml([ConfluDAO]::toXML($this.page))           
 
         $url = $this.base_url + "/" + $this.page_id + "/child/attachment"
@@ -669,6 +672,50 @@ class OTCalDAO {
         else {
             [OTCalDAO]::syukujitsu = Import-Csv "$PSScriptRoot\syukujitsu.csv" -Encoding Default 
         }
+    }
+}
+class Term {
+    [datetime] $base
+    [datetime] $start
+    [datetime] $end
+    
+    Term([datetime]$_base) {
+        $this.base = $_base
+        $this.start = Get-Date($_base.toString("yyyy/MM/dd"))
+        $this.end = $this.start.addDays(1)
+    }
+    Term([datetime]$_base, [string]$span) {
+        # span 1:month, 2:half
+        $this.base = $_base
+        switch ($span) {
+            "1" {
+                $this.start = Get-Date($_base.toString("yyyy/MM/1"))
+                $this.end = $this.start.addMonths(1)
+            }
+            "2" {
+                $diff = switch ($_base.Month) { { (4 -le $_) -and ($_ -le 9) } { 4 }; default { 10 } } 
+                $this.start = Get-Date($_base.AddMonths($diff - $_base.Month).toString("yyyy/MM/1"))
+                $this.end = $this.start.AddMonths(6)
+            }
+            default {}
+        } 
+    }
+    [boolean] Contains([datetime]$dt) {
+        return ($this.start -le $dt) -and ($dt -lt $this.end)
+    }
+    [Term] ThisMonth() {
+        return New-Object Term($this.base, 1)
+    }
+    [Term] PrevMonth() {
+        return New-Object Term($this.base.addMonts(-1), 1)
+    }
+    [Term] Half() {
+        return New-Object Term($this.base, 2)
+    }
+    [Term[]] HalfMonths() {
+        $diff = switch ($this.base.Month) { { (4 -le $_) -and ($_ -le 9) } { 4 }; default { 10 } } 
+        $diff -= $this.base.Month
+        return $diff..0 | % { New-Object Term($this.base.addmonths($_), 1) }
     }
 }
 function isHoliday([datetime]$date) {
