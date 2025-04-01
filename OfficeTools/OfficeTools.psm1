5dbba3cafcd53df0334bea59351571de80f3639b
ShumaMorimoto <shumamorimoto@gmail.com>
2025-04-01 15:50:34 +0900

    Update OfficeTools.psm1

--- OfficeTools/OfficeTools.psm1
+++ OfficeTools/OfficeTools.psm1
@@ -262,7 +262,12 @@ class ExTable :AbstractTable {
         $obj = [ordered]@{}
         foreach ($key in $item.Keys) {
             if ($item.$key -is [int]) {
-                $obj.Add($key, $this.sheet.Cells($row, $item.$key).Text)
+                $cell = $this.sheet.Cells($row, $item.$key)
+                if($cell.Hyperlinks.Count -eq 0){
+                    $obj.Add($key, $cell.Text)
+                } else {
+                    $obj.Add($key, @{Text= $cell.Text; Address = $cell.HyperLinks[1].Address})
+                }
             }
             else {
                 $obj.Add($key, $this.getData($row, $item.$key))
