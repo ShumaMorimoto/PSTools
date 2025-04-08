88bec24950d75476753aa7c5e4922e03ce1267bb
ShumaMorimoto <shumamorimoto@gmail.com>
2025-04-08 17:57:58 +0900

    日付クラス拡張

--- OfficeTools/OfficeTools.psm1
+++ OfficeTools/OfficeTools.psm1
@@ -1020,6 +1020,40 @@ function isHoliday([datetime]$date) {
     $holiday = ([OTCalDAO]::syukujitsu | Where-Object "国民の祝日・休日月日" -eq $date.ToString("yyyy/M/d"))."国民の祝日・休日名称"
     return $holiday
 }
+#
+# 日付クラスの拡張
+#
+$AddWorkDays = {
+    param([int]$days)
+    $idx = switch ($days -gt 0) { $true { 1 }; $false { -1 } }
+    $d2 = Get-Date($this)
+    while ($days -ne 0) {
+        $d2 = $d2.AddDays($idx)
+        while (-not $d2.isWorkDay) {
+            $d2 = $d2.AddDays($idx)
+        }
+        $days -= $idx
+    }
+    return $d2
+}
+$isWorkDay = {
+    return ((0, 6) -notcontains $this.DayOfWeek.value__) -and (-not $this.isHoliday) 
+}
+$Holiday = {
+    if ([OTCalDAO]::syukujitsu -eq $null) { [OTCalDAO]::loadSyukujitsu() }
+    $datestring = $this.ToString("yyyy/M/d")
+    $holiday = ([OTCalDAO]::syukujitsu | Where-Object "国民の祝日・休日月日" -eq $datestring)."国民の祝日・休日名称"
+    return $holiday
+}
+$isHoliday = {
+    return $this.Holiday -ne $null
+}
+Get-TypeData -TypeName System.DateTime | Remove-TypeData
+Update-TypeData -TypeName System.DateTime -MemberType ScriptProperty -MemberName Holiday -Value $Holiday
+Update-TypeData -TypeName System.DateTime -MemberType ScriptProperty -MemberName isHoliday -Value $isHoliday
+Update-TypeData -TypeName System.DateTime -MemberType ScriptMethod -MemberName AddWorkDays -Value $AddWorkDays
+Update-TypeData -TypeName System.DateTime -MemberType ScriptProperty -MemberName isWorkDay -Value $isWorkDay
+
 function datenormalizer {
     param([string]$val1, [string]$val2)
     
