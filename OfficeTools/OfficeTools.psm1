662418f12e25b326303e9f86cc6745a368726856
ShumaMorimoto <shumamorimoto@gmail.com>
2025-04-22 21:21:16 +0900

    Update OfficeTools.psm1

--- OfficeTools/OfficeTools.psm1
+++ OfficeTools/OfficeTools.psm1
@@ -818,6 +818,108 @@ class ConfluDAO : OTDomDAO {
         ConvertTo-JSON $settings | Set-Content $file
     }
 } 
+class TsTaskDao {
+    [string]$taskName
+    [string]$taskPath
+    [xml]$xml
+
+    TsTaskDao($taskName, $taskPath) {
+        $this.taskName = $taskName
+        $this.taskPath = $taskPath
+        $this.xml = [xml](Export-ScheduledTask -TaskName $this.taskName -TaskPath $this.taskPath)       
+    }
+    RemoveAllTrigger() {
+        $this.xml.SelectSingleNode("//*[local-name()='Triggers']").RemoveAll()
+    }
+    AppendMonthlyTrigger ([string]$TriggerTime, [string[]]$Days) {
+        $ns = $this.xml.Task.NamespaceURI
+            
+        # 新しいトリガを作成
+        $newTrigger = $this.xml.CreateElement("CalendarTrigger", $ns)
+     
+        $startBoundary = $this.xml.CreateElement("StartBoundary", $ns)
+        $startBoundary.InnerText = ([datetime]$TriggerTime).ToString("yyyy-MM-ddTHH:mm:ss")
+        $newTrigger.AppendChild($startBoundary)
+    
+        $scheduleByMonth = $this.xml.CreateElement("ScheduleByMonth", $ns)
+        $daysOfMonth = $this.xml.CreateElement("DaysOfMonth", $ns)
+        $days | % {
+            $day = $this.xml.CreateElement("Day", $ns)
+            $day.InnerText = $_
+            $daysOfMonth.AppendChild($day)
+        }
+        $scheduleByMonth.AppendChild($daysOfMonth)
+        $months = $this.xml.CreateElement("Months", $ns)
+        ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ) | % {
+            $month = $this.xml.CreateElement($_, $ns)
+            $months.AppendChild($month)
+        }
+        $scheduleByMonth.AppendChild($months)
+        $newTrigger.AppendChild($scheduleByMonth)
+        
+        # トリガを追加
+        $this.xml.SelectSingleNode("//*[local-name()='Triggers']").AppendChild($newTrigger)
+    }
+    AppendWeeklyTrigger ([string]$TriggerTime, [string[]]$days) {
+        $ns = $this.xml.Task.NamespaceURI
+            
+        # 新しいトリガを作成
+        $newTrigger = $this.xml.CreateElement("CalendarTrigger", $ns)
+     
+        $startBoundary = $this.xml.CreateElement("StartBoundary", $ns)
+        $startBoundary.InnerText = ([datetime]$TriggerTime).ToString("yyyy-MM-ddTHH:mm:ss")
+        $newTrigger.AppendChild($startBoundary)
+    
+        $scheduleByWeek = $this.xml.CreateElement("ScheduleByWeek", $ns)
+
+        $weeksInterval = $this.xml.CreateElement("WeeksInterval", $ns)
+        $weeksInterval.InnerText = "1"
+        $scheduleByWeek.AppendChild($weeksInterval)
+
+        $daysOfWeek = $this.xml.CreateElement("DaysOfWeek", $ns)
+        $days | % {
+            $day = $this.xml.CreateElement($_, $ns)
+            $daysOfWeek.AppendChild($day)
+        }
+        $scheduleByWeek.AppendChild($daysOfWeek)
+
+        $newTrigger.AppendChild($scheduleByWeek)
+        
+        # トリガを追加
+        $this.xml.SelectSingleNode("//*[local-name()='Triggers']").AppendChild($newTrigger)
+    }
+}
+class OTTaskSchedulerDAO {
+    [string]$taskPath ="\マイタスク\"
+    [TsTaskDAO[]]$table
+
+    OtTaskSchedulerDAO($taskPath){
+        $this.taskPath = $taskPath
+        $this.GetTasks()
+    }
+    OtTaskSchedulerDAO(){
+        $this.GetTasks()
+    }
+    Register ([TsTaskDAO]$task){
+        $settings = getCred
+        Register-ScheduledTask -TaskName $task.taskName -TaskPath $this.taskPath -Xml $task.xml.OuterXml -User $settings.id -Password $settings.password -Force  
+    }
+    SetTrigger([TsTaskDAO]$task,[ciminstance]$trigger){
+        $settings = getCred
+        Set-ScheduledTask -TaskName $task.taskName -TaskPath $this.taskPath -Trigger $trigger -User $settings.id -Password $settings.password  
+    }
+    GetTasks(){
+        $this.table = Get-ScheduledTask -TaskPath $this.taskPath | %{New-Object TsTaskDAO($_.TaskName, $this.taskPath)}
+    }
+    ReRegisterAll(){
+        $settings = getCred
+        foreach($task in $this.table){
+            Set-ScheduledTask -TaskName $task.taskName -TaskPath $this.taskPath -User $settings.id -Password $settings.password  
+        }
+    }
+}
+
 function getCred() {
     $file = "$PSScriptRoot\OfficeTools.settings.json"
     $settings = @{}
