ae44ad9bcd5dd0b97be59aeee6ecf0fc81199b37
ShumaMorimoto <shumamorimoto@gmail.com>
2025-08-17 14:37:57 +0900

    commit

---
+++ GS/GS.psm1
@@ -0,0 +1,9 @@
+﻿#if (-not ("Google.Apis.Sheets.v4.SheetsService" -as [Type])) {
+    Add-Type -Path "$PSScriptRoot\Google.Apis.Core.dll"
+    Add-Type -Path "$PSScriptRoot\Google.Apis.Auth.dll"
+    Add-Type -Path "$PSScriptRoot\Google.Apis.dll"
+    Add-Type -Path "$PSScriptRoot\Google.Apis.Sheets.v4.dll"
+#}
+
+
+[Google.Apis.Auth.OAuth2.GoogleCredential] -as [type]
