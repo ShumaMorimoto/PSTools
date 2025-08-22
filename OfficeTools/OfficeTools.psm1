565da0857d1b79da1262066d9deee3b530f896b9
ShumaMorimoto <shumamorimoto@gmail.com>
2025-08-23 00:43:33 +0900

    Update OfficeTools.psm1

--- OfficeTools/OfficeTools.psm1
+++ OfficeTools/OfficeTools.psm1
@@ -11,6 +11,111 @@ class OTConst {
             New-Item -Path $([OTConst]::confPath) -ItemType Directory -Force -ErrorAction Stop | Out-Null
         } catch {
         }
+<<<<<<< HEAD
+=======
+        else {
+            # デフォルト設定をPSCustomObjectとして作成し、静的プロパティに代入
+            [OTConfig]::Settings = [ordered]@{
+                Mattermost  = [ordered]@{url = "https://mattermost.aslead.cloud/api/v4" }
+                Confluence  = [ordered]@{url = "https://sd10.aslead.cloud/wiki/rest/pat/latest/tokens" }
+                LastUpdated = (Get-Date)
+            }
+            # ファイルに保存
+            [OTConfig]::Save()
+        }
+    }
+    static [void] Load() {
+        [OTConfig]::Settings = Get-Content -Path ([OTConfig]::confFile) -Raw | ConvertFrom-Json -AsHashtable
+        $cred = [OTConfig]::Settings.Credential
+        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((ConvertTo-SecureString $cred.password))
+        [OTConfig]::password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstr)
+    }
+    static [void] Save() {
+        [OTConfig]::Settings.LastUpdated = (Get-Date)
+        [OTConfig]::Settings | ConvertTo-Json -Depth 5 | Set-Content -Path ([OTConfig]::confFile)
+    }
+    static [object] GetCred() {
+        $cred = [OTConfig]::Settings.Credential
+        if (($null -eq $cred) -or ([OTConfig]::Settings.LastUpdated -lt (Get-Date).addMonths(-6))) {
+            $cred = [OTConfig]::SetCred()
+        }
+        $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((ConvertTo-SecureString $cred.password))
+        [OTConfig]::password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstr)
+        return $cred
+    } 
+    static [object] SetCred() {
+        $cred = @{}
+        $empNo = Read-Host "社員コードは？(ex.x1234)"
+        $_cred = Get-Credential
+        $cred.add("empNo", $empNo)
+        $cred.add("id", $_cred.UserName)
+        $cred.add("password", (ConvertFrom-SecureString -SecureString $_cred.Password))
+        [OTConfig]::Settings.Credential = $cred
+        [OTConfig]::Save()
+        return $cred
+    }  
+    static [string] GetMtmPAT() {
+        $pat = [OTConfig]::Settings.Mattermost.pat
+        if ($null -eq $pat) {
+            $pat = [OTConfig]::SetMtmPAT()
+        }
+        return $pat
+    } 
+    static [string] SetMtmPAT() {
+        $pat = Read-Host "MattermostのPATは？"
+        [OTConfig]::Settings.Mattermost = @{pat = $pat }
+        [OTConfig]::Save()
+        return $pat
+    }
+    static [string] GetCnflToken() {
+        $tokens = [OTConfig]::Settings.Confluence.tokens
+        if ($null -eq $tokens) {
+            $tokens = [OTConfig]::SetCnflToken()
+        }
+        if (
+            ($null -eq $tokens.expiringAt) -or `
+            ([Datetime]($tokens.expiringAt) -lt (Get-Date).AddDays(20))
+        ) {
+            $tokens = [OTConfig]::UpdateCnflToken($tokens.rawToken)
+        }
+        return $tokens
+    } 
+    static [object] UpdateCnflToken([string]$token) {
+        $headers = @{
+            "Authorization" = "Bearer $token"
+            "Content-Type"  = "application/json; charset=UTF-8"
+        }
+        $body = @{
+            name               = "myToken"
+            expirationDuration = 90
+        }
+        $json = ConvertTo-JSON -Compress $body
+        $tokens = Invoke-RestMethod -Uri ([OTConfig]::Settings.Confluence.url) -Body $json -Method "POST" -Headers ($headers)
+        [OTConfig]::Settings.Confluence.tokens = $tokens
+        [OTConfig]::Save()
+        return $tokens
+    }
+    static [object] SetCnflToken() {
+        $base64AuthInfo = [Convert]::ToBase64String( `
+                [Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f `
+                    ([OTConfig]::Settings.Credential.id -replace "cu.nri.co.jp", "nri.co.jp"), `
+                        [OTConfig]::password))`
+        )
+
+        $headers = @{
+            Authorization  = ("Basic {0}" -f $base64AuthInfo)
+            "Content-Type" = "application/json; charset=UTF-8"
+        }   
+        $body = @{
+            name               = "myToken"
+            expirationDuration = 90
+        }
+        $json = ConvertTo-JSON -Compress $body
+        $tokens = Invoke-RestMethod -Uri ([OTConfig]::Settings.Confluence.url) -Body $json -Method "POST" -Headers ($headers)
+        [OTConfig]::Settings.Confluence.tokens = $tokens
+        [OTConfig]::Save()
+        return $tokens
+>>>>>>> parent of f577701 (MattermostPAT)
     }
 }
 [OTConst]::initialize()
